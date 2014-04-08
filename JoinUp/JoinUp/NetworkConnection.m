c3e8aa9e996016a43d5aebfb2f3d3f0ad76c2696
andrew <a.fedotov@brosuniverse.com>
2014-04-08 14:31:27 +0400

    ура

--- JoinUp/JoinUp/NetworkConnection.m
+++ JoinUp/JoinUp/NetworkConnection.m
@@ -15,8 +15,9 @@ NSString *const NS_ARG_ID = @"id=";
 NSString *const NS_ARG_LONGITUDE = @"longitude=";
 NSString *const NS_ARG_LATITUDE = @"latitude=";
 NSString *const NS_ARG_PHONEID = @"tel_id=";
-//1//2//1///1//
+//1//2//3///4//
 //2
+//привет работй лучше
 NSString *const NS_KEY_ID = @"id";
 NSString *const NS_KEY_USERS = @"users";
 
